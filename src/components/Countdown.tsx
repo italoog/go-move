9a8dec2d065ef6527e9fdf911dd648e23036511a
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-02-24 23:36:30 -0300

    context api

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -6,6 +6,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function Countdown() {
   const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -28,6 +29,9 @@ export function Countdown() {
       countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000)
+    }else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
   }, [isActive, time]);
 
@@ -45,27 +49,34 @@ export function Countdown() {
         </div>
       </div>
 
-      {isActive ? (
+      {hasFinished ? (
         <button
-          type="button"
-          className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
-          onClick={resetCountdown}
+          disabled
+          className={styles.countdownButton}
         >
-          Abandonar ciclo
+          Ciclo encerrado
         </button>
       ) : (
-        <button
-          type="button"
-          className={styles.countdownButton}
-          onClick={startCountdown}
-        >
-          Iniciar um ciclo
-        </button>
-      )}
-
-      
-
-      
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      ) }
     </div>
   )
 }
